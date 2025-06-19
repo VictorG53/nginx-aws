d28372e27f635f2d8c6850fe65edca387d0f42da
Victor GIRAULT <victor.girault536@gmail.com>
2025-06-19 14:44:25 +0200

    Add gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -154,7 +154,7 @@ resource "aws_instance" "web" {
   }
 
   provisioner "local-exec" {
-    command = "echo instance public :${self.public_ip} > ip.txt"
+    command = "echo ${self.public_ip} > ip-public.txt"
   }
 }
 
@@ -179,8 +179,37 @@ resource "aws_instance" "private" {
   }
 
   provisioner "local-exec" {
-    command = "echo instance private :${self.private_ip} >> ip.txt"
+    command = "echo ${self.private_ip} >> ip-private.txt"
   }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Table de routage publique
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Association de la table de routage publique au subnet public
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
 }
 
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "instance_ip" {
   value = aws_instance.web.public_ip
 }
-
-output "private_instance_private_ip" {
-  value = aws_instance.private.private_ip
-}
